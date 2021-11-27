5df53c7c1dc7dcf71bae01d44af3ffe171b0b7d0
Richard Pineo <Richard.E.Pineo@ge.com>
2021-11-26 22:13:56 -0800

    swiftformat

--- Sources/AOCLib/Solver/PuzzleSolver.swift
+++ Sources/AOCLib/Solver/PuzzleSolver.swift
@@ -5,7 +5,7 @@ public protocol PuzzleSolver {
 	// Tries to solve part A and B
 	func solveA() -> String
 	func solveB() -> String
-	
+
 	// The correct answers for part A and B, if known
 	var answerA: String { get }
 	var answerB: String { get }
@@ -23,7 +23,7 @@ class EmptySolver: PuzzleSolver {
 	func solveB() -> String {
 		""
 	}
-	
+
 	var answerA = ""
 	var answerB = ""
 
