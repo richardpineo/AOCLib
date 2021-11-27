7c93bcd10509b3399c03061ff8b9f5576b4eb285
Richard Pineo <Richard.E.Pineo@ge.com>
2021-11-26 19:33:48 -0800

    add test

---
+++ Tests/AOCLibTests/TestMathHelper.swift
@@ -0,0 +1,9 @@
+import XCTest
+import AOCLib
+
+class TestMathHelper: XCTestCase {
+	func testLcm() throws {
+		XCTAssertEqual(24, MathHelper.lcm(of: [1, 2, 8, 3]))
+		XCTAssertEqual(252, MathHelper.lcm(of: [2, 7, 3, 9, 4]))
+	}
+}
