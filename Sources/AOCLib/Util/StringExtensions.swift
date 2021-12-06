fedc740d8be563f38d7d3cc5ea5b1d0a92b6b895
Richard Pineo <Richard.E.Pineo@ge.com>
2021-12-05 22:00:25 -0800

    ranges

--- Sources/AOCLib/Util/StringExtensions.swift
+++ Sources/AOCLib/Util/StringExtensions.swift
@@ -46,3 +46,28 @@ public extension String {
 		Insecure.MD5.hash(data: data(using: .utf8) ?? Data())
 	}
 }
+
+
+extension StringProtocol {
+	public func index<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? {
+		range(of: string, options: options)?.lowerBound
+	}
+	public func endIndex<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? {
+		range(of: string, options: options)?.upperBound
+	}
+	public func indices<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Index] {
+		ranges(of: string, options: options).map(\.lowerBound)
+	}
+	public func ranges<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Range<Index>] {
+		var result: [Range<Index>] = []
+		var startIndex = self.startIndex
+		while startIndex < endIndex,
+			let range = self[startIndex...]
+				.range(of: string, options: options) {
+				result.append(range)
+				startIndex = range.lowerBound < range.upperBound ? range.upperBound :
+					index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
+		}
+		return result
+	}
+}
