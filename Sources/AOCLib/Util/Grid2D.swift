4a4e718b3c2973c483207407365785fe9c7db65c
Richard Pineo <Richard.E.Pineo@ge.com>
2021-12-11 16:40:55 -0800

    extend grid

--- Sources/AOCLib/Util/Grid2D.swift
+++ Sources/AOCLib/Util/Grid2D.swift
@@ -6,7 +6,7 @@ public struct Grid2D {
 		let values = FileHelper.load(fileName)!.filter { !$0.isEmpty }
 		self.init(intValues: values)
 	}
-	
+
 	public init(intValues: [String]) {
 		maxPos = .init(intValues[0].count, intValues.count)
 		values = []
@@ -26,22 +26,26 @@ public struct Grid2D {
 	public func value(_ pos: Position2D) -> Int {
 		values[pos.arrayIndex(numCols: maxPos.x)]
 	}
-	
+
 	public mutating func setValue(_ pos: Position2D, _ i: Int) {
 		values[pos.arrayIndex(numCols: maxPos.x)] = i
 	}
 	
+	public func allSatisfy(_ pred: (Int) -> Bool ) -> Bool {
+		values.allSatisfy{ pred($0) }
+	}
+
 	public func neighbors(_ pos: Position2D, includePos: Bool, includeDiagonals: Bool = false) -> [Position2D] {
 		pos.neighbors(includeSelf: includePos)
 			.filter { includeDiagonals || $0.cityDistance(pos) < 2 }
 			.filter { valid($0) }
 	}
-	
+
 	public var debugDisplay: String {
 		var s = ""
 		for y in 0 ..< maxPos.y {
-		for x in 0 ..< maxPos.x {
-				s += String(value(.init(x,y)))
+			for x in 0 ..< maxPos.x {
+				s += String(value(.init(x, y)))
 			}
 			s += "\n"
 		}
