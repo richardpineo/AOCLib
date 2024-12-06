bca61ba15bea6432a1115effebbfecbbb6e5a33c
Richard Pineo <Richard.E.Pineo@ge.com>
2024-12-05 22:54:54 -0800

    public

--- Sources/AOCLib/Util/Bearing.swift
+++ Sources/AOCLib/Util/Bearing.swift
@@ -1,18 +1,18 @@
 @DebugDescription
 public struct Bearing: Hashable {
-	var position: Position2D
-	var heading: Heading
+	public var position: Position2D
+	public var heading: Heading
 	
 	// Does not change the heading of the original bearing.
-	func offset(_ inDirection: Heading, _ distance: Int = 1) -> Bearing {
+	public func offset(_ inDirection: Heading, _ distance: Int = 1) -> Bearing {
 		.init(position: position.offset(inDirection, distance), heading: heading)
 	}
 	
-	func move(_ distance: Int = 1) -> Bearing {
+	public func move(_ distance: Int = 1) -> Bearing {
 		.init(position: position.offset(heading, distance), heading: heading)
 	}
 	
-	var debugDescription: String {
+	public var debugDescription: String {
 		"Bearing \(heading) at \(position.displayString)"
-	  }
+	}
 }
